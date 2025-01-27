81c3a227f3838dfd572845be5551a893c7ddcd1a
polina4096 <polina4096@yandex.ru>
2025-01-27 06:21:37 +0300

    Fix `<Spoiler>` being broken on safari

--- src/components/Spoiler.tsx
+++ src/components/Spoiler.tsx
@@ -5,14 +5,14 @@ export function Spoiler(props: {
   placeholder: JSX.Element;
 }) {
   return (
-    <details open class="flex relative [&_.placeholder]:open:hidden [&_.chevron]:hover:text-neutral-400 [&_.chevron]:open:hover:text-text [&_.chevron]:open:i-lucide-chevron-down">
-      <summary class="list-none flex w-full select-none cursor-pointer">
+    <details open class="flex justify-start [&_.placeholder]:open:hidden [&_.chevron]:hover:text-neutral-400 [&_.chevron]:open:hover:text-text [&_.chevron]:open:i-lucide-chevron-down">
+      <summary class="list-none flex select-none cursor-pointer">
         <div class="chevron i-lucide-chevron-right text-neutral-500 mr-2 mt-1" />
         <div class="placeholder">
           {props.placeholder}
         </div>
       </summary>
-      <div class="absolute top-0 left-0 ml-6">
+      <div class="top-0 left-0">
         {props.children}
       </div>
     </details>
