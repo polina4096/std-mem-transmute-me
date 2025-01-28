ae081ff8f46e7d347ee20064fc55f7cceca05b99
polina4096 <polina4096@yandex.ru>
2025-01-28 12:09:36 +0300

    Introduce `<ToggleSpoiler>`

---
+++ src/components/ToggleSpoiler.tsx
@@ -0,0 +1,25 @@
+import type { JSX } from "solid-js";
+
+export function ToggleSpoiler(props: {
+  children: JSX.Element;
+  placeholder: JSX.Element;
+}) {
+  return (
+    <details class="[&_.indicator-close]:open:block [&_.indicator-open]:open:hidden">
+      <summary class="relative cursor-pointer select-none select-none list-none text-neutral-500 hover:text-text">
+        <div
+          class="absolute flex pr-1.5 -left-6"
+        >
+          <div>[</div>
+          <div class="indicator-open block w-2 text-center">+</div>
+          <div class="indicator-close hidden w-2 text-center">-</div>
+          <div>]</div>
+        </div>
+        <div>
+          Show more options...
+        </div>
+      </summary>
+      {props.children}
+    </details>
+  );
+}
