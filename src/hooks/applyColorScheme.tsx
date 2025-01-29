c0b62fdd417a09ed24d074b3e56b76b45c75c422
polina4096 <polina4096@yandex.ru>
2025-01-29 22:33:21 +0300

    Better color theme support, add some meta tags, refactor css, change title

--- src/hooks/applyColorScheme.tsx
+++ src/hooks/applyColorScheme.tsx
@@ -2,6 +2,23 @@ import type { Accessor } from "solid-js";
 import type { ColorScheme } from "../components/ColorSchemeContext";
 import { createEffect } from "solid-js";
 
+function createThemeColorMeta(kind?: "light" | "dark") {
+  const meta = document.createElement("meta");
+  meta.setAttribute("name", "theme-color");
+
+  switch (kind) {
+    case "light":
+      meta.setAttribute("media", "(prefers-color-scheme: light)");
+      break;
+
+    case "dark":
+      meta.setAttribute("media", "(prefers-color-scheme: dark)");
+      break;
+  }
+
+  return meta;
+}
+
 export function applyColorScheme(colorScheme: Accessor<ColorScheme>) {
   const colorSchemeClass = () => {
     switch (colorScheme()) {
@@ -21,5 +38,31 @@ export function applyColorScheme(colorScheme: Accessor<ColorScheme>) {
   createEffect(() => {
     const classes = colorSchemeClass() ?? "";
     document.documentElement.classList.value = classes;
+
+    document.head.querySelectorAll("meta[name=theme-color]").forEach(e => e.remove());
+
+    switch (colorScheme()) {
+      case "system": {
+        document.head.appendChild(createThemeColorMeta("light")).setAttribute("content", window.getComputedStyle(window.document.body).getPropertyValue("--color-light-bg"));
+        document.head.appendChild(createThemeColorMeta("dark")).setAttribute("content", window.getComputedStyle(window.document.body).getPropertyValue("--color-dark-bg"));
+        break;
+      }
+
+      case "dark": {
+        const themeColorMeta = document.head.querySelector("meta[name=theme-color]") ?? document.head.appendChild(createThemeColorMeta());
+        const style = window.getComputedStyle(window.document.body);
+        const color = style.getPropertyValue("--color-dark-bg");
+        themeColorMeta.setAttribute("content", color);
+        break;
+      }
+
+      case "light": {
+        const themeColorMeta = document.head.querySelector("meta[name=theme-color]") ?? document.head.appendChild(createThemeColorMeta());
+        const style = window.getComputedStyle(window.document.body);
+        const color = style.getPropertyValue("--color-light-bg");
+        themeColorMeta.setAttribute("content", color);
+        break;
+      }
+    }
   });
 }
