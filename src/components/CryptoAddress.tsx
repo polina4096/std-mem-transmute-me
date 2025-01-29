95f8fac73341ed5f2cb33888a4fa02facd73e507
polina4096 <polina4096@yandex.ru>
2025-01-29 19:16:37 +0300

    Lint fix

--- src/components/CryptoAddress.tsx
+++ src/components/CryptoAddress.tsx
@@ -4,7 +4,7 @@ export function CryptoAddress(props: { address: string }) {
   return (
     <div
       tabindex="0"
-      class="focus:bg-selection cursor-pointer truncate rounded-md bg-neutral-100 px-1 font-serif active:bg-neutral-300 dark:bg-neutral-900 hover:underline [&:not(:focus)]:hover:bg-neutral-200 active:selection:bg-transparent dark:active:bg-neutral-700 dark:[&:not(:focus)]:hover:bg-neutral-800"
+      class="cursor-pointer truncate rounded-md bg-neutral-100 px-1 font-serif active:bg-neutral-300 dark:bg-neutral-900 focus:bg-selection hover:underline [&:not(:focus)]:hover:bg-neutral-200 active:selection:bg-transparent dark:active:bg-neutral-700 dark:[&:not(:focus)]:hover:bg-neutral-800"
       onClick={(e) => {
         selectElement(e.target);
 
