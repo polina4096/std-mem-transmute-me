fdb52fb9ecef562ec0e8ec0b057b5f67e0e7e25c
polina4096 <polina4096@yandex.ru>
2025-01-31 12:24:13 +0300

    Add tooltip with latin spelling and IPA pronunciation to cyrillic name

--- src/ui/sections/InfoSection.tsx
+++ src/ui/sections/InfoSection.tsx
@@ -4,7 +4,12 @@ export function InfoSection() {
   return (
     <Spoiler placeholder={<div class="text-neutral-500">Expand description</div>}>
       <div class="flex flex-col gap-3 font-serif">
-        <div>Hi, I'm Полина.</div>
+        <div>
+          Hi, I'm
+          {" "}
+          <span class="underline underline-offset-2 decoration-dotted" title="Polina [pɐˈlʲinə]">Полина</span>
+          .
+        </div>
         <div>Rust enthusiast, full-stack software developer. I work on all kinds of things, yet game development, graphics and compiler development are my favorite.</div>
       </div>
     </Spoiler>
