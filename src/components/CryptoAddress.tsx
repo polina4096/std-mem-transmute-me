d61eac5db749e964c9425542606e609f9af767e4
polina4096 <polina4096@yandex.ru>
2025-02-01 14:58:03 +0300

    Fix `<CryptoAddress>` indicator button color with light theme due to improper css styling

--- src/components/CryptoAddress.tsx
+++ src/components/CryptoAddress.tsx
@@ -30,7 +30,7 @@ export function CryptoAddress(props: {
   }
 
   return (
-    <div class="relative min-w-0 [&_.active-icon]:hover:bg-neutral-100">
+    <div class="relative min-w-0 [&_.active-icon]:hover:bg-text">
       <div
         tabindex="0"
         class=":uno: cursor-pointer truncate rounded-md bg-neutral-100 px-1 font-serif active:bg-neutral-300 dark:bg-neutral-900 focus:bg-selection hover:underline [&:not(:focus)]:hover:bg-neutral-200 active:selection:bg-transparent dark:active:bg-neutral-700 dark:[&:not(:focus)]:hover:bg-neutral-800"
@@ -40,7 +40,7 @@ export function CryptoAddress(props: {
       </div>
       <Show when={props.showCopyIcon ?? true}>
         <div
-          class={clsx("active-icon bg-transparent hover:bg-neutral-100 cursor-pointer absolute top-1/2 translate-y-[-50%] -right-6 pl-2 box-content", active() ? "i-lucide-check" : "i-lucide-copy")}
+          class={clsx("active-icon bg-transparent cursor-pointer absolute top-1/2 translate-y-[-50%] -right-6 pl-2 box-content", active() ? "i-lucide-check" : "i-lucide-copy")}
           onClick={handleClick}
         />
       </Show>
