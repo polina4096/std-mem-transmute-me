8aed211366461f7a93727243a74c07319b6b126f
polina4096 <polina4096@yandex.ru>
2026-02-18 14:49:08 +0300

    More padding fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ export function Header(props: {
   return (
     <div class="relative relative relative flex items-center text-2xl font-medium [&_.anchor]:hover:block target:[&_.title]:border-r-solid target:[&_.title]:bg-yellow-200/20 target:[&_.title]:dark:bg-yellow-200/10">
       <a class="anchor absolute left-[-20px] hidden w-[20px] cursor-pointer pb-2 text-text decoration-none hover:block" href={`#${props.href ?? href()}`}>§</a>
-      <div id={props.href ?? href()} class={clsx("title w-full border-r-2 border-orange-400 pb-2 pl-1 -ml-1", props.separator && separatorClasses)}>{props.title}</div>
+      <div id={props.href ?? href()} class={clsx("title w-full border-r-2 border-orange-400 pb-2", props.separator && separatorClasses)}>{props.title}</div>
     </div>
   );
 }
